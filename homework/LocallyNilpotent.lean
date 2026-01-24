f7b343c7d74bb2b6eea4e2e36785b1c8025d6a8f
haraki-argon <2150864207@qq.com>
2026-01-24 11:52:01 +0800

    Submit the proof of 10.32.5

--- homework/LocallyNilpotent.lean
+++ homework/LocallyNilpotent.lean
@@ -1,9 +1,4 @@
-import Mathlib.RingTheory.Nilpotent.Basic
-import Mathlib.RingTheory.Ideal.Basic
-import Mathlib.RingTheory.Ideal.Maps
-import Mathlib.RingTheory.Ideal.Quotient.Defs
-import Mathlib.RingTheory.Noetherian.Defs
-import Mathlib.RingTheory.Ideal.Operations
+import Mathlib
 def Ideal.IsLocallyNilpotent {R : Type*} [Semiring R] (I : Ideal R) : Prop :=
   ∀ x ∈ I, IsNilpotent x
 
@@ -21,14 +16,33 @@ lemma isUnit_iff_isUnit_quotient {R : Type*} [CommRing R] (I : Ideal R)
     IsUnit x ↔ IsUnit ((Ideal.Quotient.mk I) x) := by sorry
 
 /-lemma 10.32.5-/
-lemma exists_pow_subset_of_radical_subset {R : Type*} [CommRing R] [IsNoetherian R R]
+/-lemma exists_pow_subset_of_radical_subset {R : Type*} [CommRing R] [IsNoetherian R R]
     (I J : Ideal R) (hJ : J ≤ I.radical) : ∃ n : ℕ, J ^ n ≤ I := by
-  sorry
+  sorry-/
+
+/-This is declared by Ideal.exists_pow_le_of_le_radical_of_fg_radical-/
 
 /-corollary-/
 lemma Ideal.isLocallyNilpotent_iff_isNilpotent {R : Type*} [CommRing R] [IsNoetherian R R]
     (I : Ideal R) : IsLocallyNilpotent I ↔ IsNilpotent I := by
-  sorry
+  constructor
+  · intro hI
+    have h : I ≤ radical ⊥ := by
+      intro x hx
+      exact hI x hx
+    have h' : ((⊥ : Ideal R).radical).FG := by
+      apply IsNoetherian.noetherian
+    have hr : ∃ k, I ^ k ≤ ⊥ := exists_pow_le_of_le_radical_of_fg_radical h h'
+    rcases hr with ⟨k, hk⟩
+    exact ⟨k, le_bot_iff.mp hk⟩
+  · intro hI x hx
+    rcases hI with ⟨n, hn⟩
+    have : x ^ n ∈ I ^ n := by
+      apply Ideal.pow_mem_pow hx n
+    rw [hn] at this
+    simp only [Submodule.zero_eq_bot, Submodule.mem_bot] at this
+    exact ⟨n, this⟩
+
 
 /-lemma 10.32.6-/
 /-However, I choose to use def instead of lemma, since the ≃ gives better results you can use-/
